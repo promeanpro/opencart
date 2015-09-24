cd0ebd46262f830d9a55d69cf71cbe702b67a17c
ubuntu <ubuntu@ubuntu1404>
2015-09-24 14:50:54 +0000

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://core3.amzaev2.dev.nemo.tv/admin/');
+define('HTTP_CATALOG', 'http://core3.amzaev2.dev.nemo.tv/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://core3.amzaev2.dev.nemo.tv/admin/');
+define('HTTPS_CATALOG', 'http://core3.amzaev2.dev.nemo.tv/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ubuntu/wwwroot/tmp2/www/admin/');
+define('DIR_SYSTEM', '/home/ubuntu/wwwroot/tmp2/www/system/');
+define('DIR_LANGUAGE', '/home/ubuntu/wwwroot/tmp2/www/admin/language/');
+define('DIR_TEMPLATE', '/home/ubuntu/wwwroot/tmp2/www/admin/view/template/');
+define('DIR_CONFIG', '/home/ubuntu/wwwroot/tmp2/www/system/config/');
+define('DIR_IMAGE', '/home/ubuntu/wwwroot/tmp2/www/image/');
+define('DIR_CACHE', '/home/ubuntu/wwwroot/tmp2/www/system/cache/');
+define('DIR_DOWNLOAD', '/home/ubuntu/wwwroot/tmp2/www/system/download/');
+define('DIR_UPLOAD', '/home/ubuntu/wwwroot/tmp2/www/system/upload/');
+define('DIR_LOGS', '/home/ubuntu/wwwroot/tmp2/www/system/logs/');
+define('DIR_MODIFICATION', '/home/ubuntu/wwwroot/tmp2/www/system/modification/');
+define('DIR_CATALOG', '/home/ubuntu/wwwroot/tmp2/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Buthahng0yee');
+define('DB_DATABASE', 'core3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://core3.amzaev2.dev.nemo.tv/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://core3.amzaev2.dev.nemo.tv/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ubuntu/wwwroot/tmp2/www/catalog/');
+define('DIR_SYSTEM', '/home/ubuntu/wwwroot/tmp2/www/system/');
+define('DIR_LANGUAGE', '/home/ubuntu/wwwroot/tmp2/www/catalog/language/');
+define('DIR_TEMPLATE', '/home/ubuntu/wwwroot/tmp2/www/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ubuntu/wwwroot/tmp2/www/system/config/');
+define('DIR_IMAGE', '/home/ubuntu/wwwroot/tmp2/www/image/');
+define('DIR_CACHE', '/home/ubuntu/wwwroot/tmp2/www/system/cache/');
+define('DIR_DOWNLOAD', '/home/ubuntu/wwwroot/tmp2/www/system/download/');
+define('DIR_UPLOAD', '/home/ubuntu/wwwroot/tmp2/www/system/upload/');
+define('DIR_MODIFICATION', '/home/ubuntu/wwwroot/tmp2/www/system/modification/');
+define('DIR_LOGS', '/home/ubuntu/wwwroot/tmp2/www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Buthahng0yee');
+define('DB_DATABASE', 'core3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
